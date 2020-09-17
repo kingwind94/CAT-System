42b955fc99dca7734533c3662a9ce45cbcbebc59
songxiongfeng <1152553924@qq.com>
2020-09-17 14:21:10 -0500

    Updates

---
+++ precache-manifest.afb85b92f38b1a1c1faa19694e5c68be.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7fb628ebfb972f8366620f9198b6c6",
+    "url": "/CAT-System/index.html"
+  },
+  {
+    "revision": "37c68767b5f98ac4f5d4",
+    "url": "/CAT-System/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "ab8fb10e9d320b65e041",
+    "url": "/CAT-System/static/css/main.1bf76d10.chunk.css"
+  },
+  {
+    "revision": "37c68767b5f98ac4f5d4",
+    "url": "/CAT-System/static/js/2.6caaabe6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CAT-System/static/js/2.6caaabe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8fb10e9d320b65e041",
+    "url": "/CAT-System/static/js/main.53de91b1.chunk.js"
+  },
+  {
+    "revision": "096583803766f7b54e83",
+    "url": "/CAT-System/static/js/runtime-main.819a6e5c.js"
+  },
+  {
+    "revision": "d93dad471f4fd90c6f20ee100cd24636",
+    "url": "/CAT-System/static/media/Adult1.d93dad47.png"
+  },
+  {
+    "revision": "7ca2e6662880b9f2112c65636cdcc974",
+    "url": "/CAT-System/static/media/Adult2.7ca2e666.png"
+  },
+  {
+    "revision": "24a70169f93322c323b55ec97544bcc6",
+    "url": "/CAT-System/static/media/Comprehending_S_Example_1.24a70169.JPG"
+  },
+  {
+    "revision": "70c9703fba0f54f56864f2f95c60dc1b",
+    "url": "/CAT-System/static/media/Comprehending_S_Example_2.70c9703f.JPG"
+  },
+  {
+    "revision": "c52d13a0de5918b9d9080e1fe137d13c",
+    "url": "/CAT-System/static/media/Comprehending_S_Example_3.c52d13a0.JPG"
+  },
+  {
+    "revision": "e298a52892db76ce26032b9f76fbddc5",
+    "url": "/CAT-System/static/media/Comprehending_S_Example_4.e298a528.JPG"
+  },
+  {
+    "revision": "3ab38885715032d543ea8773e71995aa",
+    "url": "/CAT-System/static/media/Comprehending_S_Syntax_pic_1a.3ab38885.JPG"
+  },
+  {
+    "revision": "8412a35d67a64c0aab0d0b7dc2a83d92",
+    "url": "/CAT-System/static/media/Comprehending_S_Syntax_pic_1b.8412a35d.JPG"
+  },
+  {
+    "revision": "c35c02259757f2b620e6212fdf5e3b98",
+    "url": "/CAT-System/static/media/Comprehending_S_Syntax_pic_1c.c35c0225.JPG"
+  },
+  {
+    "revision": "f2bbb5f64f178206409215407c69007d",
+    "url": "/CAT-System/static/media/Comprehending_S_Syntax_pic_1d.f2bbb5f6.JPG"
+  },
+  {
+    "revision": "666cdceac712697a265449da87270da4",
+    "url": "/CAT-System/static/media/Connect_pic_asaconsequence_1.666cdcea.jpg"
+  },
+  {
+    "revision": "ea13de32bd3cd14d19b2bc3e4d500ff2",
+    "url": "/CAT-System/static/media/Connect_pic_asaconsequence_2.ea13de32.jpg"
+  },
+  {
+    "revision": "79bbf0b07d3a5ef6c084f7b7b62ecae0",
+    "url": "/CAT-System/static/media/Connect_pic_asaresult_1.79bbf0b0.jpg"
+  },
+  {
+    "revision": "f3e0f47b88edf9cc48d83d088a72aa33",
+    "url": "/CAT-System/static/media/Connect_pic_asaresult_2.f3e0f47b.jpg"
+  },
+  {
+    "revision": "34345a475a98363cc058d2cfe1ca2eda",
+    "url": "/CAT-System/static/media/Connect_pic_consequently_1.34345a47.jpg"
+  },
+  {
+    "revision": "996cda4f69ade350b466a1e306f903fe",
+    "url": "/CAT-System/static/media/Connect_pic_forinstance_1.996cda4f.jpg"
+  },
+  {
+    "revision": "b9f3dfe66af0f4458e6daaec3a7913e2",
+    "url": "/CAT-System/static/media/Connect_pic_forinstance_2.b9f3dfe6.jpg"
+  },
+  {
+    "revision": "7d5f69105f4d278f41bdd48e451b2167",
+    "url": "/CAT-System/static/media/Connect_pic_inconclusion_1.7d5f6910.jpg"
+  },
+  {
+    "revision": "ab8e0edcd7652296611f16cb9a744030",
+    "url": "/CAT-System/static/media/Connect_pic_incontrast_1.ab8e0edc.jpg"
+  },
+  {
+    "revision": "bc87fe6dffb1b0c92eea4a407c761646",
+    "url": "/CAT-System/static/media/Connect_pic_incontrast_2.bc87fe6d.jpg"
+  },
+  {
+    "revision": "8b815babc8bc896088eefcff39d3782a",
+    "url": "/CAT-System/static/media/Connect_pic_incontrast_B_A.8b815bab.png"
+  },
+  {
+    "revision": "46e7858793b95326f568c783b5e46e31",
+    "url": "/CAT-System/static/media/Connect_pic_incontrast_B_B.46e78587.png"
+  },
+  {
+    "revision": "f7be44630a80d6fc0f670e5d09c4d3ed",
+    "url": "/CAT-System/static/media/Connect_pic_inotherwords_2.f7be4463.jpg"
+  },
+  {
+    "revision": "56103ea1eb9b19652dc55fe7081f681c",
+    "url": "/CAT-System/static/media/Connect_pic_nevertheless_1.56103ea1.jpg"
+  },
+  {
+    "revision": "56a226beb3acad575b1cffb939029472",
+    "url": "/CAT-System/static/media/Connect_pic_nevertheless_2.56a226be.jpg"
+  },
+  {
+    "revision": "15f4914eaf54b5c35fa04f75f11c49a1",
+    "url": "/CAT-System/static/media/Connect_pic_onthecontrary_1.15f4914e.jpg"
+  },
+  {
+    "revision": "5fa21b83d37159a076f614ae1eb8c78a",
+    "url": "/CAT-System/static/media/Connect_pic_onthecontrary_2.5fa21b83.jpg"
+  },
+  {
+    "revision": "4463aaf90c28f18b56cf7dbc9792b4db",
+    "url": "/CAT-System/static/media/Connect_pic_similarly_1.4463aaf9.jpg"
+  },
+  {
+    "revision": "5a6cd086369ef08cde388d28f1ff0474",
+    "url": "/CAT-System/static/media/Connect_pic_similarly_2.5a6cd086.jpg"
+  },
+  {
+    "revision": "954bcab59ed8d1894fafbab7dfe3b0c3",
+    "url": "/CAT-System/static/media/Connect_pic_therefore_A_1.954bcab5.jpg"
+  },
+  {
+    "revision": "c50848abab8a9154f88613b683b42ea4",
+    "url": "/CAT-System/static/media/Connect_pic_therefore_A_2.c50848ab.jpg"
+  },
+  {
+    "revision": "98970c2cb368e11aad909ce3084284d9",
+    "url": "/CAT-System/static/media/Connect_pic_therefore_B_1.98970c2c.jpg"
+  },
+  {
+    "revision": "d1afd58bb00112792072eace0db2462b",
+    "url": "/CAT-System/static/media/Connect_pic_therefore_B_2.d1afd58b.jpg"
+  },
+  {
+    "revision": "f1c8c0a9325e5de2af3c13d0615db1da",
+    "url": "/CAT-System/static/media/Connect_pic_whereas_A_1.f1c8c0a9.jpg"
+  },
+  {
+    "revision": "52ecba2e13d5f7fd1e4876feff1415cd",
+    "url": "/CAT-System/static/media/Connect_pic_whereas_A_2.52ecba2e.jpg"
+  },
+  {
+    "revision": "807d134e1152d6568df13cab94f074ae",
+    "url": "/CAT-System/static/media/Connect_pic_whereas_B_1.807d134e.jpg"
+  },
+  {
+    "revision": "a4e44fd20139957ed4341d9f3137b105",
+    "url": "/CAT-System/static/media/ConnectingIdeaExamplePic2.a4e44fd2.jpg"
+  },
+  {
+    "revision": "1c03edae179d8674080c5918b0d5239e",
+    "url": "/CAT-System/static/media/children1.1c03edae.png"
+  },
+  {
+    "revision": "7986c9df89a6f52f085c8befaa3de372",
+    "url": "/CAT-System/static/media/children2.7986c9df.png"
+  },
+  {
+    "revision": "6bcbf7baf8eeab71b618992ba6675286",
+    "url": "/CAT-System/static/media/play.6bcbf7ba.png"
+  }
+]);
